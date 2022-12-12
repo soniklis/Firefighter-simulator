3be4a20b7f24bb9048200a6b40c1bdc9bfd16501
soniklis <104445470+soniklis@users.noreply.github.com>
2022-12-12 10:15:43 +0100

    Fixed trash being destroyed one by one

--- Firefighter_simulator/Assets/Scripts/TrashManager.cs
+++ Firefighter_simulator/Assets/Scripts/TrashManager.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TrashManager : MonoBehaviour
 {
     public Animator CanvasAnimator;
 
+    public bool CanBeDestroyed;
+
     // Start is called before the first frame update
     void Start()
     {
         CanvasAnimator.SetBool("ObjectApproached", false);
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("e"))
+        if (Input.GetKeyDown("e") && CanBeDestroyed == true)
         {
             CanvasAnimator.SetBool("ObjectApproached", false);
             Destroy(gameObject);
@@ -25,14 +27,20 @@ public class TrashManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
+        {
             CanvasAnimator.SetBool("ObjectApproached", true);
+            CanBeDestroyed = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             CanvasAnimator.SetBool("ObjectApproached", false);
+            CanBeDestroyed = false;
+        }
     }
 
 }
